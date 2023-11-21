2c2dac76cf677a2953de8195f197f37e9d1eadb5
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-11-21 16:59:27 +0000

    trabajo hecho

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomArray = array => {
+    const randomSentence = Math.floor(Math.random() * array.length);
+    return array[randomSentence];
+  };
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "on saturday!",
+    "when i finish work!",
+    "when i was studing!",
+    "during the morning!"
+  ];
+
+  let fraseAleatoria =
+    randomArray(who) +
+    " " +
+    randomArray(action) +
+    " " +
+    randomArray(what) +
+    " " +
+    randomArray(when);
+
+  document.querySelector("#excuse").innerHTML = fraseAleatoria;
 };
